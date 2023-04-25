d77bddb04a94608e33efa8a6346e3b3c6f95cedc
mahmoud <mahmoud.ahmed.hanafy18@gmail.com>
2023-04-25 17:37:16 +0200

    add Controller

--- ECommerceMVC/ECommerceMVC/Controllers/ShoppingBagController.cs
+++ ECommerceMVC/ECommerceMVC/Controllers/ShoppingBagController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceMVC.Controllers
 {
     public class ShoppingBagController : Controller
     {
         public IActionResult Index()
-        {
+        {            
             return View();
         }
     }
