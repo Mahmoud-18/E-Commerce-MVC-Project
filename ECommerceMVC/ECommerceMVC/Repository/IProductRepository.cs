0f7e5020b3cf36e77c9de72776f3d87c09f0aab1
Moustafa-Safwat <moustafa.safwat.iti.43@gmail.com>
2023-04-25 22:50:32 +0200

    create product details page with repo and controller called product

---
+++ ECommerceMVC/ECommerceMVC/Repository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using ECommerceMVC.Models;
+
+namespace ECommerceMVC.Repository
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+    }
+}
