333a2fa4f1d820ddce0a652b9bd16cef893e6e0a
mahmoud <mahmoud.ahmed.hanafy18@gmail.com>
2023-04-25 21:09:39 +0200

    fix ProductItem and add Indix for shoppingbag

--- ECommerceMVC/ECommerceMVC/Controllers/ShoppingBagController.cs
+++ ECommerceMVC/ECommerceMVC/Controllers/ShoppingBagController.cs
@@ -1,13 +1,28 @@
-﻿using ECommerceMVC.Models;
+﻿using ECommerceMVC.Context;
+using ECommerceMVC.Models;
+using ECommerceMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceMVC.Controllers
 {
     public class ShoppingBagController : Controller
     {
+        EcommerceDbContext Context = new EcommerceDbContext();
         public IActionResult Index()
-        {            
-            return View();
+        {
+            ShoppingBagViewModel bagViewModel = new ShoppingBagViewModel();
+            bagViewModel.Items = Context.ShoppingBagItem.Include("ProductItem").ToList();
+            decimal sum = 0;
+            foreach (var item in bagViewModel.Items)
+            {
+                sum += (item.Quantity * item.ProductItem.Price);
+            }
+            bagViewModel.SubTotal = sum;
+            bagViewModel.ShippingPrice = 10;
+            bagViewModel.Total = bagViewModel.SubTotal + bagViewModel.ShippingPrice;
+
+            return View(bagViewModel);
         }
     }
 }
