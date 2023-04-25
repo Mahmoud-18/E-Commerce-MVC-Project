db561264cd467eff96fe2a37d39618d735774216
EL-Sonpaty <ahmed.sonpaty99@eng-st.cu.edu.eg>
2023-04-25 19:21:02 +0200

    25/04/2023

---
+++ ECommerceMVC/ECommerceMVC/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Submit(ContactViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            // TODO: Send email or save data to database
+
+            TempData["Message"] = "Your message has been sent. Thank you!";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ECommerceMVC/ECommerceMVC/ViewModels/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceMVC.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
