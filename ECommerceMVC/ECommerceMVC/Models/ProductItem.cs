4bf52b065631b04f82eb444c8bfc3fd922440a54
mahmoud <mahmoud.ahmed.hanafy18@gmail.com>
2023-04-26 22:12:19 +0200

    UpdateDB

--- ECommerceMVC/ECommerceMVC/Models/ProductItem.cs
+++ ECommerceMVC/ECommerceMVC/Models/ProductItem.cs
@@ -12,27 +12,18 @@ public class ProductItem
     [ForeignKey("Product")]
     public int ProductId { get; set; }
 
-    [ForeignKey("Brand")]
-    public int BrandId { get; set; }
     public decimal Price { get; set; }
     public DateTime CreatedAtUtc { get; set; }
     public DateTime? UpdatedAtUtc { get; set; }
     public DateTime? DeletedAtUtc { get; set; }
     public bool IsDeleted { get; set; } = false;
 
-    [ForeignKey("ProductType")]
-    public int ProductTypeId { get; set; }
-
     //Navigation Properties
     public Product? Product { get; set; }
-    public Brand? Brand { get; set; }
-    public ProductType? ProductType { get; set; }
+    
     public ICollection<ProductAttributeValues>? ProductAttributeValues { get; set; } = new List<ProductAttributeValues>();
     public ICollection<ShoppingBagItem>? ShoppingBagItem { get; set; } = new List<ShoppingBagItem>();
     public ICollection<OrderItems>? OrderItems { get; set; } = new List<OrderItems>();
     public ICollection<ProductImages>? ProductImages { get; set; } = new List<ProductImages>();
-
-    public ICollection<ProductImages>? ProductImages { get; set; } = new List<ProductImages>();
-
 
 }
