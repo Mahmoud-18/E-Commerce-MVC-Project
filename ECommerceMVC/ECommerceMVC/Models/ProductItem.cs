1ca27fb895c44bcfc88b10958f4c1b06d7d59ab5
EL-Sonpaty <ahmed.sonpaty99@eng-st.cu.edu.eg>
2023-04-26 17:07:57 +0200

    02

--- ECommerceMVC/ECommerceMVC/Models/ProductItem.cs
+++ ECommerceMVC/ECommerceMVC/Models/ProductItem.cs
@@ -31,5 +31,7 @@ public class ProductItem
     public ICollection<ShoppingBagItem>? ShoppingBagItem { get; set; } = new List<ShoppingBagItem>();
     public ICollection<OrderItems>? OrderItems { get; set; } = new List<OrderItems>();
 
+    public ICollection<ProductImages>? ProductImages { get; set; } = new List<ProductImages>();
+
 
 }
