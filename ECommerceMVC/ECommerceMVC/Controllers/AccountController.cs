70d4b6ac26f2b52a833f3548b48793a8f2bf3dd8
mahmoud <mahmoud.ahmed.hanafy18@gmail.com>
2023-04-27 17:12:32 +0200

    Add AccountController

---
+++ ECommerceMVC/ECommerceMVC/Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+﻿using ECommerceMVC.Context;
+using ECommerceMVC.Models;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<Customer> userManager;
+        private readonly SignInManager<Customer> signInManager;
+        private readonly EcommerceDbContext context;
+        public AccountController
+            (UserManager<Customer> _userManager,
+            SignInManager<Customer> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                Customer userModel = new Customer();
+                Address address = new Address();
+                userModel.UserName = newUser.UserName;                
+                userModel.PasswordHash = newUser.Password;
+                userModel.Email = newUser.Email;
+                userModel.Gender = newUser.Gender;
+                userModel.PhoneNumber = newUser.PhoneNumber;
+                userModel.LastName = newUser.LastName;
+                userModel.FirstName = newUser.FirstName;
+                userModel.CountryId = newUser.CountryId;
+                userModel.CreatedOnUtc = DateTime.UtcNow;
+                userModel.DataOfBirth = newUser.DataOfBirth;
+                address.Address1 = newUser.Address;
+                address.CreatedOnUtc = DateTime.UtcNow;                
+                address.City = newUser.City;
+                address.State = newUser.State;
+                address.CountryId = newUser.CountryId;
+                //save db
+                IdentityResult result =
+                    await userManager.CreateAsync(userModel, newUser.Password);   //Block create user in db
+
+                if (result.Succeeded)
+                {
+                    address.CustomerId = userModel.Id;
+                    context.Address.Add(address);
+                    await context.SaveChangesAsync();
+                    //await userManager.AddClaimAsync(userModel, new Claim("xyz","23"));
+                    //await userManager.AddToRoleAsync(userModel, "Admin");
+                    //------------------Create Cookie Authora
+                    await signInManager.SignInAsync(userModel, false);//create cookie //create cookie client
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);//Div
+                    }
+                }
+            }
+            return View(newUser);
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel userVM)
+        {
+            if (ModelState.IsValid)
+            {
+                //Check Valid User ==>db
+                Customer userModel =
+                    await userManager.FindByNameAsync(userVM.UserName);
+                if (userModel != null)
+                {
+                    bool found = await userManager.CheckPasswordAsync(userModel, userVM.Password);
+                    if (found)
+                    {
+                        //cookie
+                        await signInManager.SignInAsync(userModel, userVM.RememberMe);
+                        return RedirectToAction("Home", "Index");
+                    }
+                }
+                ModelState.AddModelError("", "Login Fail Data wrong");
+
+            }
+            return View(userVM);
+        }
+    }
+}
---
+++ ECommerceMVC/ECommerceMVC/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,39 @@
+﻿using ECommerceMVC.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceMVC.ViewModels;
+
+public class RegisterViewModel
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; }
+    public int CountryId { get; set; }
+    public string Address { get; set; }
+
+    public string City { get; set; }
+
+    public string State { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string ZipCode { get; set; }
+
+    public string CountryDialCode { get; set; }
+
+    public DateTime DataOfBirth { get; set; }
+    public Gender Gender { get; set; }
+
+
+}
