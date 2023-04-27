a84ae638898b4f7e13baab6d1fd2b346e5385b60
Moustafa-Safwat <moustafa.safwat.iti.43@gmail.com>
2023-04-27 21:02:53 +0200

    work

--- ECommerceMVC/ECommerceMVC/Controllers/ProductController.cs
+++ ECommerceMVC/ECommerceMVC/Controllers/ProductController.cs
@@ -8,6 +8,7 @@ namespace ECommerceMVC.Controllers;
 public class ProductController : Controller
 {
     IProductRepository productRepository;
+    int Id { get; set; }
     public ProductController(IProductRepository _productRepository)
     {
         productRepository = _productRepository;
@@ -15,22 +16,36 @@ public class ProductController : Controller
 
     public IActionResult ProductDetails(int id)
     {
-        ProductDetailsViewModel productDetailsViewModel = new ProductDetailsViewModel();
+        if (Id != 0)
+        {
+            Id = id;
+        }
+        if (id == 0)
+        {
+            return Redirect("http://localhost:5136/product/productdetails" + Id);
+        }
+        else
+        {
+            ProductDetailsViewModel productDetailsViewModel = new ProductDetailsViewModel();
 
-        Product product = productRepository.GetProductById(id);
-        ProductItem productItem = productRepository.GetProductItemById(id);
-        Brand brand = productRepository.GetBrandById(id);
-        //List<ProductImages> productImages = context.ProductImages.Where(im => im.ProductItemId == productItem.Id).ToList();
+            Product product = productRepository.GetProductById(id);
+            ProductItem productItem = productRepository.GetProductItemById(id);
+            Brand brand = productRepository.GetBrandById(id);
+            List<string> productImages = productRepository.GetImageById(id);
+            //List<ProductImages> productImages = context.ProductImages.Where(im => im.ProductItemId == productItem.Id).ToList();
 
-        productDetailsViewModel.Name = product.Name;
-        productDetailsViewModel.price = (float)productItem.Price;
-        productDetailsViewModel.Image = product.Image;// productImages.Select(img => img.ImageURL).ToList();
-        productDetailsViewModel.Description = product.Description;
-        productDetailsViewModel.CreatetAtUtc = productItem.CreatedAtUtc;
-        productDetailsViewModel.SUK = productItem.SKU;
-        productDetailsViewModel.StockUnits = productItem.StockQuantity;
-        productDetailsViewModel.BrandName = brand.Name;
-        productDetailsViewModel.BrandImage = brand.Image;
-        return View(productDetailsViewModel);
+            productDetailsViewModel.Name = product.Name;
+            productDetailsViewModel.price = (float)productItem.Price;
+            productDetailsViewModel.Image = productImages;// productImages.Select(img => img.ImageURL).ToList();
+            productDetailsViewModel.Description = product.Description;
+            productDetailsViewModel.CreatetAtUtc = productItem.CreatedAtUtc;
+            productDetailsViewModel.SUK = productItem.SKU;
+            productDetailsViewModel.StockUnits = productItem.StockQuantity;
+            productDetailsViewModel.BrandName = brand.Name;
+            productDetailsViewModel.BrandImage = brand.Image;
+            return View("ProductDetails", productDetailsViewModel);
+        }
     }
+
+
 }
