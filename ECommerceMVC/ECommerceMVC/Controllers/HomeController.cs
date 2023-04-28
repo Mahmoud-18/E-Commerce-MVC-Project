740b5fac4b8208a793e5c2ee3583a9639bc7158b
asmaagehad <asmaagehad75@gmail.com>
2023-04-28 23:27:04 +0200

    search

--- ECommerceMVC/ECommerceMVC/Controllers/HomeController.cs
+++ ECommerceMVC/ECommerceMVC/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using ECommerceMVC.Models;
+﻿using ECommerceMVC.Context;
+using ECommerceMVC.Models;
+using ECommerceMVC.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -9,16 +11,21 @@ namespace ECommerceMVC.Controllers
         private readonly ILogger<HomeController> _logger;
 
      
-        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        IProductRepository _productRepository;
+        public HomeController(IProductRepository productRepository)
         {
-            _logger = logger;
+            _productRepository = productRepository;
         }
-        
         public IActionResult Index()
         {
-            return View();
+            var products = _productRepository.GetAllProducts();
+            return View(products);
         }
-
+     
         public IActionResult Privacy()
         {
             return View();
@@ -29,5 +36,7 @@ namespace ECommerceMVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+       
+
     }
 }
