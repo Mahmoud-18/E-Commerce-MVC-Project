8ee4fb09c4bc30b2941037f76f1084bd2f50248b
mahmoud <mahmoud.ahmed.hanafy18@gmail.com>
2023-04-29 12:26:18 +0300

    fix admin layout and add table style

--- ECommerceMVC/ECommerceMVC/Controllers/AdminController.cs
+++ ECommerceMVC/ECommerceMVC/Controllers/AdminController.cs
@@ -1,16 +1,128 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ECommerceMVC.Models;
+using ECommerceMVC.Repository;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
 using System.Data;
+using System.Security.Claims;
 
 namespace ECommerceMVC.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly UserManager<Customer> userManager;
+        private readonly SignInManager<Customer> signInManager;
+        private readonly RoleManager<IdentityRole<int>> roleManager;        
+        IAddressRepository addressRepo;
+        IShoppingBagRepository shopBagRepository;
+        ICountryRepository country;
+
+        public AdminController
+            (UserManager<Customer> _userManager,SignInManager<Customer> _signInManager,
+            IAddressRepository _addressRepository, IShoppingBagRepository shopBagRepository,
+            ICountryRepository countryRepository, RoleManager<IdentityRole<int>> _roleManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+            addressRepo = _addressRepository;
+            this.shopBagRepository = shopBagRepository;
+            country = countryRepository;
+            roleManager = _roleManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        #region Customer(Users) Controllers
+        public IActionResult CustomerIndex() 
+        {
+            var Users = userManager.Users.ToList();            
+            return View("UsersPage", Users);
+        }
+        public IActionResult AddCustomer()
+        {
+            RegisterViewModel register = new RegisterViewModel();           
+            register.Countries = country.GetAll();
+            register.Roles = roleManager.Roles.ToList();
+            return View(register);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddCustomer(RegisterViewModel newUser)
+        {
+            List<Claim> claims = new List<Claim>();
+            if (ModelState.IsValid)
+            {               
+                Customer userModel = new Customer();
+                Address address = new Address();
+                ShoppingBag shoppingBag = new ShoppingBag();
+                userModel.UserName = newUser.UserName;
+                userModel.PasswordHash = newUser.Password;
+                userModel.Email = newUser.Email;
+                userModel.Gender = newUser.Gender;
+                userModel.PhoneNumber = newUser.PhoneNumber;
+                userModel.LastName = newUser.LastName;
+                userModel.FirstName = newUser.FirstName;
+                userModel.CountryId = newUser.CountryId;
+                userModel.CreatedOnUtc = DateTime.UtcNow;
+                userModel.DataOfBirth = newUser.DataOfBirth;
+                address.Address1 = newUser.Address;
+                address.CreatedOnUtc = DateTime.UtcNow;
+                address.City = newUser.City;
+                address.State = newUser.State;
+                address.CountryId = newUser.CountryId;
+                userModel.IsAdmin = newUser.IsAdmin;
+
+                //save db
+                IdentityResult result =
+                    await userManager.CreateAsync(userModel, newUser.Password);   //Block create user in db
+
+                if (result.Succeeded)
+                {
+                    shoppingBag.CustomerId = userModel.Id;
+                    address.CustomerId = userModel.Id;
+                    addressRepo.Insert(address);
+                    shopBagRepository.Insert(shoppingBag);
+
+                    // add Claims
+                    Claim fullnameClaim = new Claim("FullName", $"{userModel.FirstName} {userModel.LastName}");
+                    Claim firstNameClaim = new Claim("FirstName", $"{userModel.FirstName}");
+                    Claim emailClaim = new Claim(ClaimTypes.Email, userModel.Email, ClaimValueTypes.Email);
+                    Claim idClaim = new Claim("UserId", $"{userModel.Id}");
+                    claims.Add(fullnameClaim);
+                    claims.Add(firstNameClaim);
+                    claims.Add(emailClaim);
+                    claims.Add(idClaim);
+
+                    await userManager.AddClaimsAsync(userModel, claims);
+
+                    if (userModel.IsAdmin == true)
+                    {
+                       await userManager.AddToRoleAsync(userModel, "Admin");
+                    }
+                    //------------------Create Cookie Authorization
+                    await signInManager.SignInAsync(userModel, false);//create cookie //create cookie client
+                    return RedirectToAction("CustomerIndex", "Admin");
+                }
+                //else
+                //{
+                //    foreach (var item in result.Errors)
+                //    {
+                //        ModelState.AddModelError("", item.Description);
+                //    }
+                //}
+            }
+            newUser.Countries = country.GetAll();
+            newUser.Roles = roleManager.Roles.ToList();
+            return View(newUser);
+        }
+        #endregion
 
     }
 }
+    
--- ECommerceMVC/ECommerceMVC/ViewModels/RegisterViewModel.cs
+++ ECommerceMVC/ECommerceMVC/ViewModels/RegisterViewModel.cs
@@ -1,90 +1,91 @@
 ﻿using ECommerceMVC.Models;
 using ECommerceMVC.Validations;
-using Microsoft.AspNetCore.Mvc;
-
-using System.ComponentModel;
-
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
-namespace ECommerceMVC.ViewModels;
-
-public class RegisterViewModel
+namespace ECommerceMVC.ViewModels
 {
+    [Keyless]
+    public class RegisterViewModel
+    {
+        [DisplayName("First Name")]
+        [Required(ErrorMessage = "First name is required")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First name can only contain letters")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        [Required(ErrorMessage = "Last name is required")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last name can only contain letters")]
+        public string LastName { get; set; }
+
+        [DisplayName("Username")]
+        [Required(ErrorMessage = "Username is required")]       
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 50 characters")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
 
 
-    [DisplayName("First Name")]
-    [Required(ErrorMessage = "First name is required")]
-    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First name can only contain letters")]
-    public string FirstName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters")]
+        [DataType(DataType.Password)]
+        [PasswordValidation(ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one number, and no special characters.")]
+        public string Password { get; set; }
 
-    [DisplayName("Last Name")]
-    [Required(ErrorMessage = "Last name is required")]
-    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last name can only contain letters")]
-    public string LastName { get; set; }
+        [DisplayName("Confirm Password")]
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
 
-    [DisplayName("Username")]
-    [Required(ErrorMessage = "Username is required")]
-    [StringLength(50, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 50 characters")]
-    public string UserName { get; set; }
+        [DisplayName("Country")]
+        [Required(ErrorMessage = "Country is required")]
+        public int CountryId { get; set; }
 
-    [Required(ErrorMessage = "Email is required")]
-    [EmailAddress(ErrorMessage = "Invalid email address")]
-    public string Email { get; set; }
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(100, ErrorMessage = "Address must not exceed 100 characters")]
+        public string Address { get; set; }
 
-    
-    [Required(ErrorMessage = "Password is required")]    
-    [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters")]
-    [DataType(DataType.Password)]
-    [PasswordValidation(ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one number, and no special characters.")]
-    public string Password { get; set; }
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(50, ErrorMessage = "City must not exceed 50 characters")]
+        public string City { get; set; }
 
-    [DisplayName("Confirm Password")]
-    [Required(ErrorMessage = "Confirm Password is required")]
-    [Compare("Password", ErrorMessage = "Passwords do not match")]
-    [DataType(DataType.Password)]
-    public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "State is required")]
+        [StringLength(50, ErrorMessage = "State must not exceed 50 characters")]
+        public string State { get; set; }
 
-    [DisplayName("Country")]
-    [Required(ErrorMessage = "Country is required")]
-    public int CountryId { get; set; }
+        [DisplayName("Phone Number")]
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Phone number can only contain numbers")]
+        public string PhoneNumber { get; set; }
 
-    [Required(ErrorMessage = "Address is required")]
-    [StringLength(100, ErrorMessage = "Address must not exceed 100 characters")]
-    public string Address { get; set; }
+        [DisplayName("Zip Code")]
+        [Required(ErrorMessage = "Zip code is required")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Zip code can only contain numbers")]
+        public string ZipCode { get; set; }
 
-    [Required(ErrorMessage = "City is required")]
-    [StringLength(50, ErrorMessage = "City must not exceed 50 characters")]
-    public string City { get; set; }
+        //[DisplayName("Country Dial Code")]
+        //[Required(ErrorMessage = "Country dial code is required")]
+        //[RegularExpression(@"^[0-9]+$", ErrorMessage = "Country dial code can only contain numbers")]
+        //public string country_code { get; set; }
 
-    [Required(ErrorMessage = "State is required")]
-    [StringLength(50, ErrorMessage = "State must not exceed 50 characters")]
-    public string State { get; set; }
+        [DisplayName("Data of Birth")]
+        [Required(ErrorMessage = "Date of birth is required")]
+        [DataType(DataType.Date)]
+        public DateTime DataOfBirth { get; set; }
 
-    [DisplayName("Phone Number")]
-    [Required(ErrorMessage = "Phone number is required")]
-    [RegularExpression(@"^[0-9]+$", ErrorMessage = "Phone number can only contain numbers")]
-    public string PhoneNumber { get; set; }
+        [DisplayName("Gender")]
+        [Required(ErrorMessage = "Gender is required")]
+        public Gender Gender { get; set; }
 
-    [DisplayName("Zip Code")]
-    [Required(ErrorMessage = "Zip code is required")]
-    [RegularExpression(@"^[0-9]+$", ErrorMessage = "Zip code can only contain numbers")]
-    public string ZipCode { get; set; }
+        public bool IsAdmin { get; set; } = false;
+        public IList<Country>? Countries { get; set; }
 
-    //[DisplayName("Country Dial Code")]
-    //[Required(ErrorMessage = "Country dial code is required")]
-    //[RegularExpression(@"^[0-9]+$", ErrorMessage = "Country dial code can only contain numbers")]
-    //public string country_code { get; set; }
-
-    [DisplayName("Data of Birth")]
-    [Required(ErrorMessage = "Date of birth is required")]
-    [DataType(DataType.Date)]
-    public DateTime DataOfBirth { get; set; }
-
-    [DisplayName("Gender")]
-    [Required(ErrorMessage = "Gender is required")]
-    public Gender Gender { get; set; }
-
-    public IList<Country>? Countries { get; set; }
-
-
+        public IList<IdentityRole<int>>? Roles { get; set; }
+    }
+ 
 }
