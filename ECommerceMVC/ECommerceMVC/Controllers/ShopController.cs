7b907ba2db9a915e94e4f803ead7e142e5b97d16
Alzahaby <130827079+MustafaAlzahaby@users.noreply.github.com>
2023-04-30 23:27:24 +0300

    Finished Shop Page

--- ECommerceMVC/ECommerceMVC/Controllers/ShopController.cs
+++ ECommerceMVC/ECommerceMVC/Controllers/ShopController.cs
@@ -44,15 +44,41 @@ namespace ECommerceMVC.Controllers
                 this.ViewBag.Pager = pager;
                 return View(data);
             }
-
-
             //return View(products);
         }
-        public IActionResult ProductsByCategory(int id, int pg)
+        public IActionResult ProductsByCategory(int? prodsCount,int id, int pg)
         {
             ViewBag.CategoryId = id;
             var products = ProductsServices.GetProductsByCategoryId(id);
-            const int pageSize = 8;
+            int pageSize = 12;
+            if (prodsCount == null)
+            {
+                if (pg < 1)
+                    pg = 1;
+
+                int recsCounts = products.Count();
+
+                var pagers = new Pager(recsCounts, pg, pageSize);
+                int recSkips = (pg - 1) * pageSize;
+                var datas = products.Skip(recSkips).Take(pagers.PageSize).ToList();
+                this.ViewBag.Pager = pagers;
+                return View(datas);
+            }
+            else
+            {
+                pageSize = (int)prodsCount;
+                if (pg < 1)
+                    pg = 1;
+
+                int recsCounts = products.Count();
+
+                var pagers = new Pager(recsCounts, pg, pageSize);
+                int recSkips = (pg - 1) * pageSize;
+                var datas = products.Skip(recSkips).Take(pagers.PageSize).ToList();
+                this.ViewBag.Pager = pagers;
+                return View(datas);
+            }
+
             if (pg < 1)
                 pg = 1;
 
