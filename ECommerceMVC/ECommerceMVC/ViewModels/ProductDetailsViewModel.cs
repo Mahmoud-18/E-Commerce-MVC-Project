e566e658e6cc0f7d2d8aa763a18bfed1851379dd
Moustafa-Safwat <moustafa.safwat.iti.43@gmail.com>
2023-04-30 23:39:59 +0200

    dd

--- ECommerceMVC/ECommerceMVC/ViewModels/ProductDetailsViewModel.cs
+++ ECommerceMVC/ECommerceMVC/ViewModels/ProductDetailsViewModel.cs
@@ -5,12 +5,19 @@
         public string? Name { get; set; }
         public float price { get; set; }
         public float PriceBeforeDiscount { get; set; }
-        public List<string>? Image { get; set; }
+        public List<string>? Image { get; set; } = new List<string>();
+        public List<string>? Size { get; set; } = new List<string>();
+        public List<string>? Color { get; set; } = new List<string>();
         public string? Description { get; set; }
-        public DateTime CreatetAtUtc { get; set; }
-        public int SUK { get; set; }
-        public int StockUnits { get; set; }
         public string? BrandName { get; set; }
-        public string? BrandImage { get; set; }
+        //
+        public int ProductCount { get; set; }
+        public int  SizeId { get; set; }
+        public int ColorId { get; set; }
+
+        //public int SUK { get; set; }
+        //public DateTime CreatetAtUtc { get; set; }
+        //public int StockUnits { get; set; }
+        //public string? BrandImage { get; set; }
     }
 }
