f1a743751d83b81fdcf830caf4aaa2d7913d3dd1
Alzahaby <130827079+MustafaAlzahaby@users.noreply.github.com>
2023-04-30 22:54:26 +0300

    Finalized the Shop Page

--- ECommerceMVC/ECommerceMVC/Controllers/ShopController.cs
+++ ECommerceMVC/ECommerceMVC/Controllers/ShopController.cs
@@ -7,35 +7,52 @@ using Microsoft.AspNetCore.Mvc;
 namespace ECommerceMVC.Controllers
 {
     public class ShopController : Controller
-    {     
+    {
         ProductsServices ProductsServices;
 
-        public ShopController (ProductsServices productsServices)
+        public ShopController(ProductsServices productsServices)
         {
             ProductsServices = productsServices;
         }
-        public IActionResult ShowAllProducts(int pg = 1)
+        public IActionResult ShowAllProducts(int? prodsCount, int pg = 1)
         {
             var products = ProductsServices.GetAllProducts();
+            int pageSize = 12;
+            if (prodsCount == null)
+            {
+                if (pg < 1)
+                    pg = 1;
 
-            const int pageSize = 6;
-            if (pg < 1)
-                pg = 1;
+                int recsCount = products.Count();
 
-            int recsCount = products.Count();
+                var pager = new Pager(recsCount, pg, pageSize);
+                int recSkip = (pg - 1) * pageSize;
+                var data = products.Skip(recSkip).Take(pager.PageSize).ToList();
+                this.ViewBag.Pager = pager;
+                return View(data);
+            }else
+            {
+                pageSize = (int)prodsCount;
+                if (pg < 1)
+                    pg = 1;
 
-            var pager = new Pager(recsCount, pg, pageSize);
-            int recSkip = (pg - 1) * pageSize;
-            var data = products.Skip(recSkip).Take(pager.PageSize).ToList();
-            this.ViewBag.Pager = pager;
-            return View(data);
+                int recsCount = products.Count();
+
+                var pager = new Pager(recsCount, pg, pageSize);
+                int recSkip = (pg - 1) * pageSize;
+                var data = products.Skip(recSkip).Take(pager.PageSize).ToList();
+                this.ViewBag.Pager = pager;
+                return View(data);
+            }
+
+
             //return View(products);
         }
         public IActionResult ProductsByCategory(int id, int pg)
         {
             ViewBag.CategoryId = id;
             var products = ProductsServices.GetProductsByCategoryId(id);
-            const int pageSize = 6;
+            const int pageSize = 8;
             if (pg < 1)
                 pg = 1;
 
